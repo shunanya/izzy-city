7b6938d7e836494a7db3c1226a20679d939662f0
simon <simon.hunanyan@gmail.com>
2024-07-15 10:31:08 +0400

    Modifying "UserDetailsImpl" class

--- src/main/java/com/izzy/service/UserDetailsImpl.java
+++ src/main/java/com/izzy/service/UserDetailsImpl.java
@@ -11,6 +11,7 @@ import java.sql.Timestamp;
 import java.time.LocalDate;
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 public class UserDetailsImpl implements UserDetails {
@@ -84,5 +85,35 @@ public class UserDetailsImpl implements UserDetails {
 
     public String getPhoneNumber(){
         return phone_number;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
     }
 }
