b12faf53bf4d95d5c10069149ab9fc3c41942aaa
simon <simon.hunanyan@gmail.com>
2024-07-22 22:12:02 +0400

    Added Welcome page

---
+++ src/main/java/com/izzy/payload/request/OrderRequest.java
@@ -0,0 +1,163 @@
+package com.izzy.payload.request;
+
+import jakarta.validation.constraints.Size;
+
+import java.sql.Timestamp;
+import java.util.Arrays;
+
+public class OrderRequest {
+    @Size(min = 1, max = 20)
+    private String name;
+    private String description;
+    @Size(min = 3, max = 50)
+    private String action;
+    private Long createdBy;
+    private Timestamp createdAt;
+    private Long updatedBy;
+    private Timestamp updatedAt;
+    private Long assignedTo;
+    @Size(min = 3, max = 50)
+    private String status;
+    private Long takenBy;
+    private Timestamp takenAt;
+    private Timestamp doneAt;
+    private Long[] scooters;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public Long getCreatedBy() {
+        return createdBy;
+    }
+
+    public void setCreatedBy(Long createdBy) {
+        this.createdBy = createdBy;
+    }
+
+    public Timestamp getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Timestamp createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Long getUpdatedBy() {
+        return updatedBy;
+    }
+
+    public void setUpdatedBy(Long updatedBy) {
+        this.updatedBy = updatedBy;
+    }
+
+    public Timestamp getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Timestamp updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Long getAssignedTo() {
+        return assignedTo;
+    }
+
+    public void setAssignedTo(Long assignedTo) {
+        this.assignedTo = assignedTo;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Long getTakenBy() {
+        return takenBy;
+    }
+
+    public void setTakenBy(Long takenBy) {
+        this.takenBy = takenBy;
+    }
+
+    public Timestamp getTakenAt() {
+        return takenAt;
+    }
+
+    public void setTakenAt(Timestamp takenAt) {
+        this.takenAt = takenAt;
+    }
+
+    public Timestamp getDoneAt() {
+        return doneAt;
+    }
+
+    public void setDoneAt(Timestamp doneAt) {
+        this.doneAt = doneAt;
+    }
+
+    public Long[] getScooters() {
+        return scooters;
+    }
+
+    public void setScooters(Long[] scooters) {
+        this.scooters = scooters;
+    }
+
+    public enum Action {
+        MOVE("Move"), CHARGE("Charge");
+        private final String value;
+
+        Action(String value) {
+            this.value = value;
+        }
+
+        public static Boolean checkByValue(String value) {
+            return Arrays.stream(values()).filter(month -> month.getValue().equals(value)).findFirst().map(v -> !v.toString().isBlank()).orElse(Boolean.FALSE);
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+
+    public enum Status {
+        CREATED("Created"), ASSIGNED("Assigned"), IN_PROGRESS("In_Progress"), FULFILLED("Fulfilled"), CANCELED("Canceled");
+        private final String value;
+
+        Status(String value) {
+            this.value = value;
+        }
+
+        public static Boolean checkByValue(String value) {
+            return Arrays.stream(values()).filter(month -> month.getValue().equals(value)).findFirst().map(v -> !v.toString().isBlank()).orElse(Boolean.FALSE);
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+}
---
+++ src/main/java/com/izzy/payload/request/ScooterRequest.java
@@ -0,0 +1,77 @@
+package com.izzy.payload.request;
+
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Size;
+
+import java.util.Arrays;
+
+public class ScooterRequest {
+    @Size(min = 1, max = 20)
+    private String identifier;
+    private String status;
+    @Min(0) // Ensures the batteryLevel is at least 0
+    @Max(100) // Ensures the batteryLevel does not exceed 100
+    private Integer batteryLevel;
+    private String zone;
+    @Min(10) // Ensures the speedLimit is at least 10
+    @Max(100) // Ensures the speedLimit does not exceed 100
+    private Integer speedLimit;
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Integer getBatteryLevel() {
+        return batteryLevel;
+    }
+
+    public void setBatteryLevel(Integer batteryLevel) {
+        this.batteryLevel = batteryLevel;
+    }
+
+    public String getZone() {
+        return zone;
+    }
+
+    public void setZone(String zone) {
+        this.zone = zone;
+    }
+
+    public Integer getSpeedLimit() {
+        return speedLimit;
+    }
+
+    public void setSpeedLimit(Integer speedLimit) {
+        this.speedLimit = speedLimit;
+    }
+
+    public enum Status {
+        ACTIVE("Active"), INACTIVE("Inactive"), BLOCKED("Blocked"), UNBLOCKED("Unblocked"), BROKEN("Broken"), RENTED("Rented");
+        private final String value;
+
+        Status(String value) {
+            this.value = value;
+        }
+
+        public static Boolean checkByValue(String value) {
+            return Arrays.stream(values()).filter(month -> month.getValue().equals(value)).findFirst().map(v -> !v.toString().isBlank()).orElse(Boolean.FALSE);
+        }
+
+        public String getValue() {
+            return value;
+        }
+    }
+}
