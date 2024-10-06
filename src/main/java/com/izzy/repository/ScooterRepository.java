2b1e009c81f0b3c39a2769ae62762c8120b735a3
simon <simon.hunanyan@gmail.com>
2024-10-06 12:39:47 +0400

    Changes in Repository

--- src/main/java/com/izzy/repository/ScooterRepository.java
+++ src/main/java/com/izzy/repository/ScooterRepository.java
@@ -2,14 +2,50 @@ package com.izzy.repository;
 
 import com.izzy.model.Scooter;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
+import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface ScooterRepository extends JpaRepository<Scooter, Long> {
 
     Optional<Scooter> findScooterByIdentifier(@Param("identifier") @NonNull String identifier);
+
+    /**
+     * finding Scooters with a battery level within a specified range.
+     *
+     * @param minBatteryLevel The minimum battery level (inclusive) to filter the scooters.
+     * @param maxBatteryLevel The maximum battery level (inclusive) to filter the scooters
+     * @return a list of Scooters whose battery levels fall within the specified range.
+     */
+    List<Scooter> findScootersByBatteryLevelBetween(@Param("minBatteryLevel") @NonNull Integer minBatteryLevel,
+                                                    @Param("maxBatteryLevel") @NonNull Integer maxBatteryLevel);
+
+    /**
+     * finding Scooters with specified zone name
+     *
+     * @param zoneName the zone name where scooters are located
+     * @return a list of Scooters which located in the specified zone
+     */
+    List<Scooter> findScootersByZone_Name(@Param("zoneName") String zoneName);
+
+    @Query("SELECT s FROM Scooter s " +
+            "JOIN s.zone z " +  // Join with the Zone entity
+            "WHERE " +
+            "(:identifier IS NULL OR s.identifier = :identifier) AND " +
+            "(:minBatteryLevel IS Null OR :maxBatteryLevel IS NULL OR s.batteryLevel BETWEEN :minBatteryLevel AND :maxBatteryLevel) AND " +
+            "(:minSpeedLimit IS NULL OR :maxSpeedLimit IS NULL OR s.speedLimit BETWEEN :minSpeedLimit AND :maxSpeedLimit) AND " +
+            "(:status IS NULL OR s.status = :status) AND " +
+            "(:zoneName IS NULL OR z.name = :zoneName)")
+    List<Scooter> findByFilter(@Param("identifier") @Nullable String identifier,
+                               @Param("minBatteryLevel") @Nullable Integer minBatteryLevel, @Param("maxBatteryLevel") @Nullable Integer maxBatteryLevel,
+                               @Param("minSpeedLimit") @Nullable Integer minSpeedLimit, @Param("maxSpeedLimit") @Nullable Integer maxSpeedLimit,
+                               @Param("status") @Nullable String status,
+                               @Param("zoneName") @Nullable String zoneName);
+
 }
--- src/main/java/com/izzy/repository/UserRepository.java
+++ src/main/java/com/izzy/repository/UserRepository.java
@@ -9,6 +9,8 @@ import org.springframework.lang.NonNull;
 import org.springframework.lang.Nullable;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Timestamp;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -32,37 +34,31 @@ public interface UserRepository extends JpaRepository<User, Long> {
                                   @Param("phoneNumber") @Nullable String phoneNumber,
                                   @Param("gender") @Nullable String gender,
                                   @Param("shift") @Nullable String shift);
-/*
 
-    @Query("SELECT u FROM User u WHERE " +
-            "(:firstName IS NULL OR u.firstName ILIKE %:firstName%) AND " +
-            "(:lastName IS NULL OR u.lastName ILIKE %:lastName%) AND " +
-            "(:phoneNumber IS NULL OR u.phoneNumber ILIKE %:phoneNumber%) AND " +
-            "(:gender IS NULL OR u.gender ILIKE %:gender%) AND " +
-            "(:shift IS NULL OR u.shift ILIKE %:shift%) AND " +
-            "(:zone IS NULL OR u.zone IS NULL OR u.zone.name ILIKE %:zone%)")
-    List<User> findUsersByFilters(@Param("firstName") String firstName,
-                                  @Param("lastName") String lastName,
-                                  @Param("phoneNumber") String phoneNumber,
-                                  @Param("gender") String gender,
-                                  @Param("zone") String zone,
-                                  @Param("shift") String shift);
-*/
-/*
-    @Query("SELECT u FROM User u WHERE " +
-            "(:firstName IS NULL OR u.firstName ILIKE %:firstName%) AND " +
-            "(:lastName IS NULL OR u.lastName ILIKE %:lastName%) AND " +
-            "(:phoneNumber IS NULL OR u.phoneNumber ILIKE %:phoneNumber%) AND " +
+    @Query("SELECT u FROM User u JOIN u.zone z WHERE " +
+            "(:firstName IS NULL OR u.firstName = :firstName) AND " +
+            "(:lastName IS NULL OR u.lastName = :lastName) AND " +
+            "(:phoneNumber IS NULL OR u.phoneNumber = :phoneNumber) AND " +
             "(:gender IS NULL OR u.gender = :gender) AND " +
+            "(CAST(:minDateOfBirth AS date) IS NULL OR CAST(u.dateOfBirth AS date) IS NULL OR u.dateOfBirth >= :minDateOfBirth) AND " +
+            "(CAST(:maxDateOfBirth AS date) IS NULL OR CAST(u.dateOfBirth AS date) IS NULL OR u.dateOfBirth <= :maxDateOfBirth) AND " +
             "(:shift IS NULL OR u.shift = :shift) AND " +
-            "(:zone IS NULL OR u.zone.name = :zone) AND " +
-            "(:roles IS NULL OR EXISTS (SELECT r FROM u.roles r WHERE r.name IN :roles))")
-    List<User> findUsersByFilters(@Param("firstName") String firstName,
-                                  @Param("lastName") String lastName,
-                                  @Param("phoneNumber") String phoneNumber,
-                                  @Param("gender") String gender,
-                                  @Param("zone") String zone,
-                                  @Param("shift") String shift,
-                                  @Param("roles") List<String> roles);
-*/
+            "(CAST(:minCreatedAt AS timestamp) IS NULL OR CAST(u.createdAt AS timestamp) IS NULL OR u.createdAt >= :minCreatedAt) AND " +
+            "(CAST(:maxCreatedAt AS timestamp) IS NULL OR CAST(u.createdAt AS timestamp) IS NULL OR u.createdAt <= :maxCreatedAt) AND " +
+            "(:zoneName IS NULL OR z.name = :zoneName)"
+            )
+    List<User> findUsersByFiltering(@Param("firstName") @Nullable String firstName,
+                                    @Param("lastName") @Nullable String lastName,
+                                    @Param("phoneNumber") @Nullable String phoneNumber,
+                                    @Param("gender") @Nullable String gender,
+                                    @Param("minDateOfBirth") @Nullable LocalDate minDateOfBirth, @Param("maxDateOfBirth") LocalDate maxDateOfBirth,
+                                    @Param("shift") @Nullable String shift,
+                                    @Param("minCreatedAt") @Nullable Timestamp minCreatedAt, @Param("maxCreatedAt") @Nullable Timestamp maxCreatedAt,
+                                    @Param("zoneName") @Nullable String zoneName);
+
+    @Query(value = "SELECT * FROM users u WHERE " +
+            "(cast(:minCreatedAt AS timestamp) IS NULL OR u.created_at >= :minCreatedAt) AND " +
+            "(cast(:maxCreatedAt AS timestamp) IS NULL OR u.created_at <= :maxCreatedAt)",
+            nativeQuery = true)
+    List<User> findUsersCreatedBetween(@Param("minCreatedAt") @Nullable Timestamp minCreatedAt, @Param("maxCreatedAt") @Nullable Timestamp maxCreatedAt);
 }
